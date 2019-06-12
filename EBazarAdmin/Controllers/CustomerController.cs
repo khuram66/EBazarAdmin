2af9cc9a4946ccdbe0a1cd5f172982f84fbd0b2a
khuram66 <kskhurram66@gmail.com>
2019-06-12 21:10:01 +0500

    vendor and customer viewmodel and index action created for both

---
+++ EBazarAdmin/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using EBazarAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBazarAdmin.Controllers
+{
+    public class CustomerController : Controller
+    {
+        ECommerceDBEntities db = new ECommerceDBEntities();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+    }
+}
---
+++ EBazarAdmin/Controllers/VendorController.cs
@@ -0,0 +1,19 @@
+﻿using EBazarAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBazarAdmin.Controllers
+{
+    public class VendorController : Controller
+    {
+        ECommerceDBEntities db = new ECommerceDBEntities();
+        // GET: Vendor
+        public ActionResult Index()
+        {
+            return View(db.Vendors.ToList());
+        }
+    }
+}
